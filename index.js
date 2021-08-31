bfe54f5253634bc3acf444cbf5f6bfde35a77809
Goldie <goldiejackson25@yahoo.com>
2021-08-31 17:34:13 -0500

    commit tree 530c33eaf473a05d5aa9ba7b4750e001fdfad4bb

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    let blocks;
+        if(street >= 42){
+            blocks = street - 42;
+        }
+        else{
+            blocks = 42 - street;
+        }
+    return blocks;
+}
+
+//Way to condense ^
+
+//distanceFromHqInFeet
+function distanceFromHqInFeet(street){
+    return (distanceFromHqInBlocks(street) * 264);
+}
+
+
+function distanceTravelledInFeet(block1, block2){
+    let feetTravelled = (block1 > block2) ? (block1 - block2) * 264 : (block2 - block1) * 264;
+    return feetTravelled;
+}
+
+
+function calculatesFarePrice(start, destination){
+    let cost = 0;
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet <=400){
+        return cost;
+    }
+    else if (feet > 400 && feet < 2000) {
+        feet = feet - 400;
+        cost = feet * .02;
+        return cost;
+    }
+    else if (2000 < feet && feet <= 2500){
+        cost = 25;
+        return cost;
+    }
+    else if (feet > 2500){
+        return "cannot travel that far";
+    }
+}
